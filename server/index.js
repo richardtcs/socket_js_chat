b2d200287d11fc9b9a97f7942b0f5823d14f2012
richardtcs <richard.velasco@tcs.com.ve>
2022-08-05 11:50:50 -0400

    fix chat

--- server/index.js
+++ server/index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const http = require('http');
-const { Socket } = require("dgram");
 const server = http.createServer(app);
 var io = require('socket.io')(server, {
     cors: {
         origin: "*",
-    }
+    },
+    maxHttpBufferSize: 1e8 // 100Mb
 });
 
 app.use(express.json());
 app.use(cors());
 
-var clients = [];
+//var clients = [];
 var chat = io.of('/chat');
 var rooms_test = io.of('/test');
 
@@ -28,8 +28,8 @@ function checkCon(){
         test_admin.shift();
         test_clt.shift();
 
-        admin.emit('message', `Sistema Connected to User ${client.handshake.query.name}`);
-        client.emit('message', `Sistema Connected to Admin ${admin.handshake.query.name}`);
+        admin.emit('message', `Sistema Connected to Ejecutante: ${client.handshake.query.name}`);
+        client.emit('message', `Sistema Connected to Administrador: ${admin.handshake.query.name}`);
 
         admin.on('message', msg => {
             client.emit('message', `${admin.handshake.query.name} ${msg}`);
@@ -38,67 +38,105 @@ function checkCon(){
         client.on('message', msg => {
             admin.emit('message', `${client.handshake.query.name} ${msg}`);
         });
+        
+        client.on('img', img => {
+            admin.emit('img', `${client.handshake.query.name} ${img}`);
+        })
+
+        admin.on('img', img => {
+            client.emit('img', `${admin.handshake.query.name} ${img}`)
+        })
 
         admin.on('disconnect', () => {
-            client.emit('con', 'Admin disconnected');
+            client.emit('con', 'Administrador disconnected');
             client.disconnect(true);
         });
 
         client.on('disconnect', () => {
-            admin.emit('con', 'Client disconnected');
-            admin.disconnect(true);
+            if(admin.connected){
+                admin.emit('message', `Sistema ${client.handshake.query.name} se ha desconectado`);
+                test_admin.push(admin);
+            }
         });
     }
 }
 
 rooms_test.on('connection', async (client) => {
-    if(client.handshake.query.type == 'User'){
+    if(client.handshake.query.type == 'Ejecutante'){
         test_clt.push(client);
-        console.log('User connected');
+        console.log('Ejecutante connected');
         console.log(test_clt);
-    } else if(client.handshake.query.type == 'Admin') {
-        console.log('Admin connected');
+    } else if(client.handshake.query.type == 'Administrador') {
         test_admin.push(client);
+        console.log('Administrador connected');
         console.log(test_admin);
     } else {
         console.log('Unidentified user');
-        client.disconnect(true);
+        //client.disconnect(true);
+        client.on("message", msg => {
+            client.emit("test", {name: "richard", msg: "Hola"});
+        });
     }
 
     checkCon();
 
     client.on('disconnect', () => {
-        if(client.handshake.query.type == 'User'){
+        if(client.handshake.query.type == 'Ejecutante'){
+            console.log("Ejecutante disconnected");
             if(test_clt.includes(client)){
                 test_clt.splice(test_clt.indexOf(client), 1);
             }
-        } else if (client.handshake.query.type == 'Admin'){
+            console.log(test_clt);
+        } else if (client.handshake.query.type == 'Administrador'){
+            console.log("Administrador disconnected");
             if(test_admin.includes(client)){
                 test_admin.splice(test_admin.indexOf(client), 1);
             }
+            console.log(test_admin);
         }
-        console.log(test_clt);
     })
 });
 
 chat.on('connection', (client) => {
-    clients.push(client.id);
-    console.log(`Cliente ${clients.length} conectado`);
-    console.log(`Id: ${client.id} Name: ${client.handshake.query.name}`);
+    client.join('chat');
+    //clients.push(client.id);
+    console.log(`Cliente Id: ${client.id} Name: ${client.handshake.query.name} conectado`);
     
     client.on('message', msg => {
-        console.log(`${client.handshake.query.name} ${msg}`);
-        clients.map((e) => {
-            if(client.id != e){
-              client.to(e).emit("message", `${client.handshake.query.name} ${msg}`);
-              //[${clients_name[clients.indexOf(client)]}] 
-            }
+        // clients.map((e) => {
+        //     if(client.id != e){
+        //         client.to(e).emit("message", {
+        //             name: client.handshake.query.name,
+        //             msg: msg
+        //         });
+        //       //[${clients_name[clients.indexOf(client)]}] 
+        //     }
+        // });
+        client.to('chat').emit("message", {
+            name: client.handshake.query.name,
+            msg: msg
         });
     });
 
+    client.on('img', img => {
+        // clients.map((e) => {
+        //     if(client.id != e){
+        //         client.to(e).emit('img', {
+        //             name: client.handshake.query.name,
+        //             img: img
+        //         });
+        //       //[${clients_name[clients.indexOf(client)]}] 
+        //     }
+        // });
+        client.to('chat').emit('img', {
+            name: client.handshake.query.name,
+            img: img
+        });
+    })
+
     client.on('disconnect', () => {
-        console.log(`desconectado ${client.id} ${client.handshake.query.name}`)
-        clients.splice(clients.indexOf(client.id), 1);
+        console.log(`Cliente Id: ${client.id} Name: ${client.handshake.query.name} desconectado`);
+        //clients.splice(clients.indexOf(client.id), 1);
     });
 });
 
